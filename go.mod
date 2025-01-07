829e29d750b35e6c8cb6a8604de1682b918f3c50
qingw1230 <1366502108@qq.com>
2025-01-07 16:31:42 +0800

    feat: 添加服务发现

--- go.mod
+++ go.mod
@@ -4,16 +4,20 @@ go 1.19
 
 require (
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/go-zookeeper/zk v1.0.4
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/olivere/elastic/v7 v7.0.32
+	github.com/pkg/errors v0.9.1
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.19.0
+	google.golang.org/grpc v1.62.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -22,7 +26,6 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -33,7 +36,10 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240314234333-6e1732d8331c // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
---
+++ pkg/discoveryregistry/zookeeper/discover.go
@@ -0,0 +1,108 @@
+package zookeeper
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/go-zookeeper/zk"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/resolver"
+)
+
+var (
+	ErrConnIsNil               = errors.New("conn is nil")
+	ErrConnIsNilButLocalNotNil = errors.New("conn is nil, but local is not nil")
+)
+
+func (s *ZkClient) watch() {
+	for {
+		event := <-s.eventChan
+		switch event.Type {
+		case zk.EventSession:
+			s.logger.Printf("zk session event: %+v", event)
+		case zk.EventNodeChildrenChanged:
+			s.logger.Printf("zk event: %s", event.Path)
+			l := strings.Split(event.Path, "/")
+			if len(l) > 1 {
+				serviceName := l[len(l)-1]
+				s.lock.Lock()
+				s.flushResolverAndDeleteLocal(serviceName)
+				s.lock.Unlock()
+			}
+			s.logger.Printf("zk event handle success: %s", event.Path)
+		case zk.EventNodeDataChanged:
+		case zk.EventNodeCreated:
+		case zk.EventNodeDeleted:
+		case zk.EventNotWatching:
+		}
+	}
+}
+
+func (s *ZkClient) GetConnsRemote(serviceName string) (conns []resolver.Address, err error) {
+	path := s.getPath(serviceName)
+	_, _, _, err = s.conn.ChildrenW(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "children watch error")
+	}
+	childNodes, _, err := s.conn.Children(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "get children error")
+	} else {
+		for _, child := range childNodes {
+			fullPath := path + "/" + child
+			data, _, err := s.conn.Get(fullPath)
+			if err != nil {
+				if err == zk.ErrNoNode {
+					return nil, errors.Wrap(err, "this is zk ErrNoNode")
+				}
+				return nil, errors.Wrap(err, "get children error")
+			}
+			conns = append(conns, resolver.Address{Addr: string(data), ServerName: serviceName})
+		}
+	}
+	return conns, nil
+}
+
+func (s *ZkClient) GetConns(ctx context.Context, serviceName string, opts ...grpc.DialOption) ([]grpc.ClientConnInterface, error) {
+	s.logger.Printf("get conns from client, serviceName: %s", serviceName)
+	opts = append(s.options, opts...)
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	conns := s.localConns[serviceName]
+	if len(conns) == 0 {
+		var err error
+		s.logger.Printf("get conns from zk remote, serviceName: %s", serviceName)
+		addrs, err := s.GetConnsRemote(serviceName)
+		if err != nil {
+			return nil, err
+		}
+		if len(addrs) == 0 {
+			return nil, fmt.Errorf("no conn for service %s, grpc server may not exist, local conn is %v, please check zookeeper server %v, path: %s", serviceName, s.localConns, s.zkServers, s.zkRoot)
+		}
+		for _, addr := range addrs {
+			cc, err := grpc.DialContext(ctx, addr.Addr, append(s.options, opts...)...)
+			if err != nil {
+				return nil, err
+			}
+			conns = append(conns, cc)
+		}
+		s.localConns[serviceName] = conns
+	}
+	return conns, nil
+}
+
+func (s *ZkClient) GetConn(ctx context.Context, serviceName string, opts ...grpc.DialOption) (grpc.ClientConnInterface, error) {
+	newOpts := append(s.options, grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, s.balancerName)))
+	s.logger.Printf("get conn from client, serviceName: %s", serviceName)
+	return grpc.DialContext(ctx, fmt.Sprintf("%s:///%s", s.scheme, serviceName), append(newOpts, opts...)...)
+}
+
+func (s *ZkClient) CloseConn(conn grpc.ClientConnInterface) {
+	if closer, ok := conn.(io.Closer); ok {
+		closer.Close()
+	}
+}
